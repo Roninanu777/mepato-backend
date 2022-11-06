e0c72651403d38d85baaf88bc02c2a93a7de1ca4
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2022-11-06 12:26:55 +0530

    added login path

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 // routes
 //app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
-app.use("/auth", require("./routes/auth"));
+app.use("/login", require("./routes/login"));
 app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
 
